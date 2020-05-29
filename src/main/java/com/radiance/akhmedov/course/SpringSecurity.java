508157b2d217cff13ce7bc78be8f06beae7e4918
l0gark <l0gark@ya.ru>
2020-05-29 15:41:08 +0300

    spring security fix

--- src/main/java/com/radiance/akhmedov/course/SpringSecurity.java
+++ src/main/java/com/radiance/akhmedov/course/SpringSecurity.java
@@ -2,6 +2,7 @@ package com.radiance.akhmedov.course;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,6 +30,8 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
         http.csrf().disable()
                 .cors().disable()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/articles").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/balance").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
